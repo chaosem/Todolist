77eb0e6c0141a07b83db0d9b30b6ad296c871459
Will Roberts <w.liu89@gmail.com>
2017-09-21 17:01:12 -0700

    delete

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -1,8 +1,13 @@
-// Check Off specific Todos By Clicking
 $(document).ready(function() {
+    // Check Off specific Todos By Clicking
     $('li').click(function() {
         $(this).toggleClass('completed');
     })
+
+    // Click on X to delete Todo
+    $('span').click(function(event) {
+        $(this).remove();
+        event.stopPropagation();
+    });
 });
 
-// Click on X to delete Todo
